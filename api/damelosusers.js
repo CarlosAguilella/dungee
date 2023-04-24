2a6070ed90cd3035cec17a1fd0723ccf3678edfd
CarlosAguilella <114015985+CarlosAguilella@users.noreply.github.com>
2023-04-24 16:53:25 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,5 +1,5 @@
 export default function handler(request, response){
 
     fetch('https://dungee-252d3-default-rtdb.europe-west1.firebasedatabase.app/pins.json')
-    .then(jsonstring => response.send(`{"datostal": ${jsonstring} }`))
+    .then(jsonstring => response.json(jsonstring))
 }
