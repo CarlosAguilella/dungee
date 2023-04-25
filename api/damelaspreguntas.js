3e853c7b6786f555c159c6ac9a6c6f567e1b467e
CarlosAguilella <114015985+CarlosAguilella@users.noreply.github.com>
2023-04-25 17:43:08 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -1,6 +1,12 @@
 export default function handler(request, response){
     const preguntas = {
         "pregunta" : "cuanto es 2+2?"
+        "pregunta" : "cuanto es 3+2?"
+        "pregunta" : "cuanto es 4+2?"
+        "pregunta" : "cuanto es 5+2?"
+        "pregunta" : "cuanto es 6+2?"
+        "pregunta" : "cuanto es 7+2?"
+
 
     }
     response.json(preguntas)
