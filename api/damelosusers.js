32037d14618b25d0e27e8bbe5a620ec5626238a0
CarlosAguilella <114015985+CarlosAguilella@users.noreply.github.com>
2023-04-25 15:19:40 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,5 +1,5 @@
 export default function handler(request, response){
-    fetch('https://dungee-252d3-default-rtdb.europe-west1.firebasedatabase.app/pins.json')
+    fetch(`https://dungee-252d3-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}.json`)
     .then(datos => datos.json())
     .then(json => response.json(json))
 }
