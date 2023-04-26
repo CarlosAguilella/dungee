3cea0d531d4b3e0e10c52b7837de14ba022503df
CarlosAguilella <114015985+CarlosAguilella@users.noreply.github.com>
2023-04-26 16:02:18 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -9,8 +9,6 @@ export default function handler(request, response){
         "pregunta3" : {
             "pregunta" : "3+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "3"
         }
-
     }
-
     response.json(preguntas)
 }
