b8843345f5e049e44324cd667db4f86c48a21cba
CarlosAguilella <114015985+CarlosAguilella@users.noreply.github.com>
2023-04-26 17:05:11 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -1,13 +1,15 @@
 export default function handler(request, response){
-    const preguntas = {
-        "pregunta1" : {
-            "pregunta" : "2+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "1"
-        },
-        "pregunta2" : {
-            "pregunta" : "18+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "2"
-        },
-        "pregunta3" : {
-            "pregunta" : "3+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "3"
+    const questions = {
+        "preguntas" :{    
+            "pregunta0" : {
+                "pregunta" : "2+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "1"
+            },
+            "pregunta1" : {
+                "pregunta" : "18+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "2"
+            },
+            "pregunta2" : {
+                "pregunta" : "3+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "3"
+            }
         }
     }
     response.json(preguntas)
