a91ee27b08e025f323a432fe40f454205bd434da
CARLOS <carlos@example.com>
2023-05-11 20:52:33 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -5,7 +5,7 @@ export default function handler(request, response){
                 "pregunta" : "2+2?", "res1" : "4", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "1"
             },
             "pregunta1" : {
-                "pregunta" : "18+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "2"
+                "pregunta" : "Capital de FRANCIA?", "res1" : "Madrid", "res2" : "Manchester", "res3" : "Paris", "res4" : "Valencia", "correcta" : "3"
             },
             "pregunta2" : {
                 "pregunta" : "3+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "3"
@@ -14,7 +14,7 @@ export default function handler(request, response){
                 "pregunta" : "Capital de ESPAÑA?", "res1" : "Madrid", "res2" : "Manchester", "res3" : "Paris", "res4" : "Valencia", "correcta" : "1"
             },
             "pregunta4" : {
-                "pregunta" : "Capital de FRANCIA?", "res1" : "Madrid", "res2" : "Manchester", "res3" : "Paris", "res4" : "Valencia", "correcta" : "3"
+                "pregunta" : "18+2?", "res1" : "2", "res2" : "20", "res3" : "5", "res4" : "3", "correcta" : "2"
             },
             "pregunta5" : {
                 "pregunta" : "Capital de ITALIA?", "res1" : "Madrid", "res2" : "Roma", "res3" : "Paris", "res4" : "Valencia", "correcta" : "2"
